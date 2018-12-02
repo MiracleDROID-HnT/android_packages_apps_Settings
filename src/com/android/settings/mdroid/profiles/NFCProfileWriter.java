Inventor1937/packages_apps_Settings

Added system profiles [2/2]

src/com/android/settings/aospextended/profiles/NFCProfileWriter.java
@@ -1,0 +1,114 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aospextended.profiles;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.internal.util.lineageos.app.Profile;
+import com.android.internal.util.lineageos.app.ProfileManager;
+import com.android.settings.R;
+
+import java.util.UUID;
+
+/**
+ * Activity to support writing a profile to an NFC tag.
+ * The mime type is "lineage/profile" and the payload is the raw bytes of the profile's
+ * UUID. The payload was intentionally kept small to support writing on 46-byte tags.
+ */
+public class NFCProfileWriter extends Activity {
+
+    private static final String TAG = "NFCProfileWriter";
+
+    static final String EXTRA_PROFILE_UUID = "PROFILE_UUID";
+
+    private NfcAdapter mNfcAdapter;
+
+    private IntentFilter[] mWriteTagFilters;
+
+    private Profile mProfile;
+
+    private ProfileManager mProfileManager;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        mProfileManager = ProfileManager.getInstance(this);
+
+        setContentView(R.layout.nfc_writer);
+        setTitle(R.string.sp_profile_write_nfc_tag);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        String profileUuid = getIntent().getStringExtra(EXTRA_PROFILE_UUID);
+        if (profileUuid != null) {
+            mProfile = mProfileManager.getProfile(UUID.fromString(profileUuid));
+            Log.d(TAG, "Profile to write: " + mProfile.getName());
+            enableTagWriteMode();
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disableTagWriteMode();
+    }
+
+    private PendingIntent getPendingIntent() {
+        return PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    }
+
+    private void disableTagWriteMode() {
+        mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    private void enableTagWriteMode() {
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        mWriteTagFilters = new IntentFilter[] {
+                tagDetected
+        };
+        mNfcAdapter.enableForegroundDispatch(this, getPendingIntent(), mWriteTagFilters, null);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // Tag writing mode
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if (NFCProfileUtils.writeTag(NFCProfileUtils.getProfileAsNdef(mProfile), detectedTag)) {
+                Toast.makeText(this, R.string.sp_profile_write_success, Toast.LENGTH_LONG).show();
+                NFCProfileUtils.vibrate(this);
+            } else {
+                Toast.makeText(this, R.string.sp_profile_write_failed, Toast.LENGTH_LONG).show();
+            }
+            finish();
+        }
+    }
+}

src/com/android/settings/aospextended/profiles/actions/item/AirplaneModeItem.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aospextended.profiles.actions.item;
+
+import com.android.internal.util.lineageos.profiles.AirplaneModeSettings;
+import com.android.settings.aospextended.profiles.actions.ItemListAdapter;
+import com.android.settings.R;
+
+public class AirplaneModeItem extends BaseItem {
+    AirplaneModeSettings mSettings;
+
+    public AirplaneModeItem(AirplaneModeSettings airplaneModeSettings) {
+        if (airplaneModeSettings == null) {
+            airplaneModeSettings = new AirplaneModeSettings();
+        }
+        mSettings = airplaneModeSettings;
+    }
+
+    @Override
+    public ItemListAdapter.RowType getRowType() {
+        return ItemListAdapter.RowType.AIRPLANEMODE_ITEM;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getTitle() {
+        return getString(R.string.sp_profile_airplanemode_title);
+    }
+
+    @Override
+    public String getSummary() {
+        return getString(getModeString(mSettings));
+    }
+
+    public AirplaneModeSettings getSettings() {
+        return mSettings;
+    }
+
+    public static int getModeString(AirplaneModeSettings settings) {
+        if (settings.isOverride()) {
+            if (settings.getValue() == 1) {
+                return R.string.sp_profile_action_enable;
+            } else {
+                return R.string.sp_profile_action_disable;
+            }
+        } else {
+            return R.string.sp_profile_action_none;
+        }
+    }
+}

src/com/android/settings/aospextended/profiles/actions/item/TriggerItem.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.aospextended.profiles.actions.item;
+
+import com.android.internal.util.lineageos.app.Profile;
+import com.android.settings.R;
+import com.android.settings.aospextended.profiles.actions.ItemListAdapter;
+
+import java.util.ArrayList;
+
+public class TriggerItem extends BaseItem {
+
+    // from Profile.TriggerType
+    public static final int WIFI = 0;
+    public static final int BLUETOOTH = 1;
+    // not in Profile.TriggerType, but we need it.
+    public static final int NFC = 2;
+
+    Profile mProfile;
+    int mTriggerType;
+
+    public TriggerItem(Profile profile, int whichTrigger) {
+        mProfile = profile;
+        mTriggerType = whichTrigger;
+    }
+
+    @Override
+    public ItemListAdapter.RowType getRowType() {
+        return ItemListAdapter.RowType.TRIGGER_ITEM;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public int getTriggerType() {
+        return mTriggerType;
+    }
+
+    @Override
+    public String getTitle() {
+        return getString(getTitleString(mTriggerType));
+    }
+
+    @Override
+    public String getSummary() {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Profile.ProfileTrigger> triggers = mProfile.getTriggersFromType(mTriggerType);
+
+        for (int i = 0; i < triggers.size(); i++) {
+            sb.append(triggers.get(i).getName());
+            if (i < (triggers.size() - 1)) {
+                sb.append("\n");
+            }
+        }
+
+        if (sb.length() == 0) {
+            if (mTriggerType == NFC) {
+                return getString(R.string.sp_no_triggers_configured_nfc);
+            } else {
+                return getString(R.string.sp_no_triggers_configured);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static int getTitleString(int triggerType) {
+        switch (triggerType) {
+            case WIFI:
+                return R.string.sp_profile_tabs_wifi;
+            case BLUETOOTH:
+                return R.string.sp_profile_tabs_bluetooth;
+            case NFC:
+                return R.string.sp_profile_tabs_nfc;
+            default: return 0;
+        }
+    }
+}

src/com/android/settings/aospextended/profiles/utils/ScreenType.java
@@ -1,0 +1,50 @@
+package com.android.settings.aospextended.profiles.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.DisplayInfo;
+import android.view.WindowManager;
+
+public class ScreenType {
+    // Device type reference
+    private static int sDeviceType = -1;
+
+    // Device types
+    private static final int DEVICE_PHONE = 0;
+    private static final int DEVICE_HYBRID = 1;
+    private static final int DEVICE_TABLET = 2;
+
+    private static int getScreenType(Context context) {
+        if (sDeviceType == -1) {
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            DisplayInfo outDisplayInfo = new DisplayInfo();
+            wm.getDefaultDisplay().getDisplayInfo(outDisplayInfo);
+            int shortSize = Math.min(outDisplayInfo.logicalHeight, outDisplayInfo.logicalWidth);
+            int shortSizeDp = shortSize * DisplayMetrics.DENSITY_DEFAULT
+                    / outDisplayInfo.logicalDensityDpi;
+            if (shortSizeDp < 600) {
+                // 0-599dp: "phone" UI with a separate status & navigation bar
+                sDeviceType =  DEVICE_PHONE;
+            } else if (shortSizeDp < 720) {
+                // 600-719dp: "phone" UI with modifications for larger screens
+                sDeviceType = DEVICE_HYBRID;
+            } else {
+                // 720dp: "tablet" UI with a single combined status & navigation bar
+                sDeviceType = DEVICE_TABLET;
+            }
+        }
+        return sDeviceType;
+    }
+
+    public static boolean isPhone(Context context) {
+        return getScreenType(context) == DEVICE_PHONE;
+    }
+
+    public static boolean isHybrid(Context context) {
+        return getScreenType(context) == DEVICE_HYBRID;
+    }
+
+    public static boolean isTablet(Context context) {
+        return getScreenType(context) == DEVICE_TABLET;
+    }
+}

MiracleDROID-HnT/android_packages_apps_Settings

src/com/android/settings/mdroid/profiles/NFCProfileWriter.java
@@ -1,0 +1,114 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.mdroid.profiles;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.internal.util.lineageos.app.Profile;
+import com.android.internal.util.lineageos.app.ProfileManager;
+import com.android.settings.R;
+
+import java.util.UUID;
+
+/**
+ * Activity to support writing a profile to an NFC tag.
+ * The mime type is "lineage/profile" and the payload is the raw bytes of the profile's
+ * UUID. The payload was intentionally kept small to support writing on 46-byte tags.
+ */
+public class NFCProfileWriter extends Activity {
+
+    private static final String TAG = "NFCProfileWriter";
+
+    static final String EXTRA_PROFILE_UUID = "PROFILE_UUID";
+
+    private NfcAdapter mNfcAdapter;
+
+    private IntentFilter[] mWriteTagFilters;
+
+    private Profile mProfile;
+
+    private ProfileManager mProfileManager;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        mProfileManager = ProfileManager.getInstance(this);
+
+        setContentView(R.layout.nfc_writer);
+        setTitle(R.string.sp_profile_write_nfc_tag);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        String profileUuid = getIntent().getStringExtra(EXTRA_PROFILE_UUID);
+        if (profileUuid != null) {
+            mProfile = mProfileManager.getProfile(UUID.fromString(profileUuid));
+            Log.d(TAG, "Profile to write: " + mProfile.getName());
+            enableTagWriteMode();
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        disableTagWriteMode();
+    }
+
+    private PendingIntent getPendingIntent() {
+        return PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    }
+
+    private void disableTagWriteMode() {
+        mNfcAdapter.disableForegroundDispatch(this);
+    }
+
+    private void enableTagWriteMode() {
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        mWriteTagFilters = new IntentFilter[] {
+                tagDetected
+        };
+        mNfcAdapter.enableForegroundDispatch(this, getPendingIntent(), mWriteTagFilters, null);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        // Tag writing mode
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+            Tag detectedTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            if (NFCProfileUtils.writeTag(NFCProfileUtils.getProfileAsNdef(mProfile), detectedTag)) {
+                Toast.makeText(this, R.string.sp_profile_write_success, Toast.LENGTH_LONG).show();
+                NFCProfileUtils.vibrate(this);
+            } else {
+                Toast.makeText(this, R.string.sp_profile_write_failed, Toast.LENGTH_LONG).show();
+            }
+            finish();
+        }
+    }
+}

src/com/android/settings/mdroid/profiles/actions/item/AirplaneModeItem.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.mdroid.profiles.actions.item;
+
+import com.android.internal.util.lineageos.profiles.AirplaneModeSettings;
+import com.android.settings.mdroid.profiles.actions.ItemListAdapter;
+import com.android.settings.R;
+
+public class AirplaneModeItem extends BaseItem {
+    AirplaneModeSettings mSettings;
+
+    public AirplaneModeItem(AirplaneModeSettings airplaneModeSettings) {
+        if (airplaneModeSettings == null) {
+            airplaneModeSettings = new AirplaneModeSettings();
+        }
+        mSettings = airplaneModeSettings;
+    }
+
+    @Override
+    public ItemListAdapter.RowType getRowType() {
+        return ItemListAdapter.RowType.AIRPLANEMODE_ITEM;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getTitle() {
+        return getString(R.string.sp_profile_airplanemode_title);
+    }
+
+    @Override
+    public String getSummary() {
+        return getString(getModeString(mSettings));
+    }
+
+    public AirplaneModeSettings getSettings() {
+        return mSettings;
+    }
+
+    public static int getModeString(AirplaneModeSettings settings) {
+        if (settings.isOverride()) {
+            if (settings.getValue() == 1) {
+                return R.string.sp_profile_action_enable;
+            } else {
+                return R.string.sp_profile_action_disable;
+            }
+        } else {
+            return R.string.sp_profile_action_none;
+        }
+    }
+}

src/com/android/settings/mdroid/profiles/actions/item/TriggerItem.java
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2012 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.mdroid.profiles.actions.item;
+
+import com.android.internal.util.lineageos.app.Profile;
+import com.android.settings.R;
+import com.android.settings.mdroid.profiles.actions.ItemListAdapter;
+
+import java.util.ArrayList;
+
+public class TriggerItem extends BaseItem {
+
+    // from Profile.TriggerType
+    public static final int WIFI = 0;
+    public static final int BLUETOOTH = 1;
+    // not in Profile.TriggerType, but we need it.
+    public static final int NFC = 2;
+
+    Profile mProfile;
+    int mTriggerType;
+
+    public TriggerItem(Profile profile, int whichTrigger) {
+        mProfile = profile;
+        mTriggerType = whichTrigger;
+    }
+
+    @Override
+    public ItemListAdapter.RowType getRowType() {
+        return ItemListAdapter.RowType.TRIGGER_ITEM;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public int getTriggerType() {
+        return mTriggerType;
+    }
+
+    @Override
+    public String getTitle() {
+        return getString(getTitleString(mTriggerType));
+    }
+
+    @Override
+    public String getSummary() {
+        StringBuilder sb = new StringBuilder();
+        ArrayList<Profile.ProfileTrigger> triggers = mProfile.getTriggersFromType(mTriggerType);
+
+        for (int i = 0; i < triggers.size(); i++) {
+            sb.append(triggers.get(i).getName());
+            if (i < (triggers.size() - 1)) {
+                sb.append("\n");
+            }
+        }
+
+        if (sb.length() == 0) {
+            if (mTriggerType == NFC) {
+                return getString(R.string.sp_no_triggers_configured_nfc);
+            } else {
+                return getString(R.string.sp_no_triggers_configured);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static int getTitleString(int triggerType) {
+        switch (triggerType) {
+            case WIFI:
+                return R.string.sp_profile_tabs_wifi;
+            case BLUETOOTH:
+                return R.string.sp_profile_tabs_bluetooth;
+            case NFC:
+                return R.string.sp_profile_tabs_nfc;
+            default: return 0;
+        }
+    }
+}

src/com/android/settings/mdroid/profiles/utils/ScreenType.java
@@ -1,0 +1,50 @@
+package com.android.settings.mdroid.profiles.utils;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.DisplayInfo;
+import android.view.WindowManager;
+
+public class ScreenType {
+    // Device type reference
+    private static int sDeviceType = -1;
+
+    // Device types
+    private static final int DEVICE_PHONE = 0;
+    private static final int DEVICE_HYBRID = 1;
+    private static final int DEVICE_TABLET = 2;
+
+    private static int getScreenType(Context context) {
+        if (sDeviceType == -1) {
+            WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            DisplayInfo outDisplayInfo = new DisplayInfo();
+            wm.getDefaultDisplay().getDisplayInfo(outDisplayInfo);
+            int shortSize = Math.min(outDisplayInfo.logicalHeight, outDisplayInfo.logicalWidth);
+            int shortSizeDp = shortSize * DisplayMetrics.DENSITY_DEFAULT
+                    / outDisplayInfo.logicalDensityDpi;
+            if (shortSizeDp < 600) {
+                // 0-599dp: "phone" UI with a separate status & navigation bar
+                sDeviceType =  DEVICE_PHONE;
+            } else if (shortSizeDp < 720) {
+                // 600-719dp: "phone" UI with modifications for larger screens
+                sDeviceType = DEVICE_HYBRID;
+            } else {
+                // 720dp: "tablet" UI with a single combined status & navigation bar
+                sDeviceType = DEVICE_TABLET;
+            }
+        }
+        return sDeviceType;
+    }
+
+    public static boolean isPhone(Context context) {
+        return getScreenType(context) == DEVICE_PHONE;
+    }
+
+    public static boolean isHybrid(Context context) {
+        return getScreenType(context) == DEVICE_HYBRID;
+    }
+
+    public static boolean isTablet(Context context) {
+        return getScreenType(context) == DEVICE_TABLET;
+    }
+}